c1f4ef480f99d554f9859f54d766591b6fab02f5
bruno.edoh <bruno.edoh@ASH-LAB221-14M10F5.ashesi.local>
2017-04-23 22:28:15 +0000

    did the long stuff

--- src/farmi/Result_PageController.java
+++ src/farmi/Result_PageController.java
@@ -5,9 +5,15 @@
  */
 package farmi;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -16,6 +22,37 @@ import javafx.fxml.Initializable;
  */
 public class Result_PageController implements Initializable {
 
+    @FXML
+    private Label vegName;
+    @FXML
+    private Label vegWeather;
+    @FXML
+    private ImageView vegImage;
+    @FXML
+    private JFXButton back;
+    @FXML
+    private Label vegSoil;
+    @FXML
+    private Label vegHarvest;
+    @FXML
+    private JFXTextArea vegProcess;
+    @FXML
+    private Label vegPest;
+    @FXML
+    private Label vegPesticide;
+    @FXML
+    private Label vegPesticidePrice;
+    @FXML
+    private Label vegDisease;
+    @FXML
+    private Label vegDiseaseChem;
+    @FXML
+    private Label vegChemComp;
+    @FXML
+    private Label vegMarket;
+    @FXML
+    private Label vegMarketPrice;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +60,9 @@ public class Result_PageController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void goBack(ActionEvent event) {
+    }
     
 }
